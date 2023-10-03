1fe0d7579698f370bce08f669d1fef28a610f804
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-10-03 16:21:55 -0300

    Add Interface/Decorators

--- person.rb
+++ person.rb
@@ -1,9 +1,44 @@
-class Person
+require 'securerandom'
+
+class Nameable
+  def correct_name
+    raise NotImplementedError , "Message"
+  end
+end
+
+class Decorator  < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end  
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name 
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name 
+    original_name =  @nameable.correct_name
+    if original_name.length >= 10
+      original_name[0, 10].strip
+    else
+      original_name
+    end  
+  end  
+end  
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(id, age, name = 'Unknown', parent_permission: true)
-    @id = id
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = SecureRandom.uuid
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -13,9 +48,20 @@ class Person
     of_age? || @parent_permission
   end
 
+  def correct_name
+    @name
+  end  
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+# person = Person.new(22, 'maximilianus')
+# person.correct_name
+# capitalized_person = CapitalizeDecorator.new(person)
+# puts capitalized_person.correct_name
+# capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+# puts capitalized_trimmed_person.correct_name
